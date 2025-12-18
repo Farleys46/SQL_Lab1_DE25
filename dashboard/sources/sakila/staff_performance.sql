cdd3079737ed8f9c84e444290f7cc43dad27fc8b
Filip Farley <filip@farley.se>
2025-12-18 15:58:36 +0100

    update staff.md

--- dashboard/sources/sakila/staff_performance.sql
+++ dashboard/sources/sakila/staff_performance.sql
@@ -1,7 +1,6 @@
 SELECT
     staff.staff_id,
-    staff.first_name,
-    staff.last_name,
+    staff.first_name || ' ' || staff.last_name AS full_name,
     COUNT(payment.payment_id) AS number_of_payments,
     ROUND(SUM(payment.amount), 2) AS total_revenue,
     ROUND(AVG(payment.amount), 2) AS average_revenue_per_payment
@@ -17,4 +16,4 @@ GROUP BY
     staff.first_name,
     staff.last_name
 
-ORDER BY total_revenue ASC;
+ORDER BY total_revenue DESC;
